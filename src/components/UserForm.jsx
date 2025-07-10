fc7bb83d7bb0d9b2dc7695c800640df46609cf78
w1z4rd993 <isaiasjunker7@gmail.com>
2025-07-10 01:15:39 -0300

    Maquetado del formulario

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,5 +1,11 @@
+import { useState } from "react";
 
-
+// Valores por defecto del useState para userForm
+const initialUserForm = {
+    username: '',
+    password: '',
+    email: ''
+}
 /**
  * Componente para el formulario de usuario.
  * 
@@ -10,9 +16,56 @@
  * el formulario de usuario.
  */
 export const UserForm = () => {
+
+    const [userForm, setUserForm] = useState(initialUserForm);
+
+    const { username, password, email } = userForm;
+
+
+    /**
+     * Maneja el cambio de entrada en los campos del formulario de usuario.
+     * Actualiza el estado `userForm` con el nuevo valor del campo 
+     * correspondiente al nombre del campo modificado.
+     * @param {Object} target - El objeto de evento del input modificado.
+     * @param {string} target.name - El nombre del campo de entrada.
+     * @param {string} target.value - El nuevo valor del campo de entrada.
+     */
+    const onInputChange = ({ target }) => {
+
+        const { name, value } = target;
+        setUserForm({
+            ...userForm,
+            [name]: value,
+        })
+    }
+
     return (
-        <>
-            <p>Formulario de usuario</p>
-        </>
+        <form>
+            <input
+                className='form-control my-3 w-75%'
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={onInputChange} />
+            <input
+                className='form-control my-3 w-75%'
+                placeholder="Password"
+                type="password"
+                name="password"
+                value={password}
+                onChange={onInputChange} />
+            <input
+                className='form-control my-3 w-75%'
+                placeholder="Email"
+                name="email"
+                value={email}
+                onChange={onInputChange} />
+
+            <button
+                className="btn btn-primary"
+                type="submit">
+                Crear
+            </button>
+        </form>
     );
 }
