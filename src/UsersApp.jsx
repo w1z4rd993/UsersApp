9f822c24e643824c50d349107ba4e35b77717e9a
w1z4rd993 <isaiasjunker7@gmail.com>
2025-07-13 13:41:38 -0300

    Creación de ventana modal para el formulario de creación y edición de usuarios

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -24,36 +24,50 @@ export const UsersApp = () => {
     } = useUsers();
 
     return (
-        <div className="container my-4">
-            <h2>Users App</h2>
-            <div className="row">
+        <>
+            {!visibleForm || <div className="abrir-modal animacion fadeIn">
+                <div className="modal" style={ {display: "block"}} tabIndex="-1">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">
+                                    {userSelected.id > 0 ? 'Editar' : 'Crear'} Modal Usuarios
+                                </h5>
+                            </div>
+                            <div className="modal-body">
+                                <UserForm
+                                    initialUserForm={initialUserForm}
+                                    handlerAddUser={handlerAddUser}
+                                    userSelected={userSelected}
+                                    handlerCloseForm={handlerCloseForm}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>}
 
-                {!visibleForm ||
+            <div className="container my-4">
+                <h2>Users App</h2>
+                <div className="row">
+
                     <div className="col">
-                        <UserForm
-                            initialUserForm={initialUserForm}
-                            handlerAddUser={handlerAddUser}
-                            userSelected={userSelected}
-                            handlerCloseForm={handlerCloseForm}
-                        />
-                    </div>}
+                        {visibleForm || <button
+                            className="btn btn-primary my-2" type="button"
+                            onClick={handlerOpenForm}>
+                            Nuevo usuario
+                        </button>}
 
-                <div className="col">
-                    {visibleForm || <button
-                        className="btn btn-primary my-2" type="button"
-                        onClick={handlerOpenForm}>
-                        Nuevo usuario
-                    </button>}
-
-                    {users.length === 0
-                        ? <div className="alert alert-warning">No hay usuarios en el sistema</div>
-                        : <UsersList
-                            users={users}
-                            handlerUserSelectedForm={handlerUserSelectedForm}
-                            handlerRemoveUser={handlerRemoveUser}
-                        />}
+                        {users.length === 0
+                            ? <div className="alert alert-warning">No hay usuarios en el sistema</div>
+                            : <UsersList
+                                users={users}
+                                handlerUserSelectedForm={handlerUserSelectedForm}
+                                handlerRemoveUser={handlerRemoveUser}
+                            />}
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
