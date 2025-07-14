f2d32bf553dd6318b9183dcd43d3d7789d3f4bc5
w1z4rd993 <isaiasjunker7@gmail.com>
2025-07-14 09:40:29 -0300

    Implementación de los eventos para el login

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,9 +1,9 @@
-import { UsersPage } from "./pages/UsersPage";
+import { LoginPage } from "./auth/pages/LoginPage";
 
 export const UsersApp = () => {
     return (
         <>
-        <UsersPage />
+        <LoginPage />
         </>
     );
 }
