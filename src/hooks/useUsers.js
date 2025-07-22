5dd9d111dccd60ec92877dff886b6ffbb8b91acf
w1z4rd993 <isaiasjunker7@gmail.com>
2025-07-22 10:21:33 -0300

    Validación de campos únicos

--- src/hooks/useUsers.js
+++ src/hooks/useUsers.js
@@ -67,6 +67,17 @@ export const useUsers = () => {
         } catch (error) {
             if (error.response && error.response.status === 400) {
                 setErrors(error.response.data);
+            } else if (error.response && error.response.status === 500 &&
+                error.response.data?.message?.includes("constraint")) {
+
+                if (error.response.data?.message?.includes("UK_username")) {
+                    setErrors({ username: "El username ya existe" });
+                }
+
+                if (error.response.data?.message?.includes("UK_email")) {
+                    setErrors({ email: "El email ya existe" });
+                }
+
             } else {
                 throw error;
             }
